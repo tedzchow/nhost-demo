a298cbdf83d1739ad6fbc87bcfeeedbb43c330f8
Ted Chow <ted.z.chow@gmail.com>
2023-02-13 05:02:00 -0500

    Added insert todo

--- index.js
+++ index.js
@@ -16,20 +16,37 @@ const request = (async () => {
     throw signInResponse;
   }
 
-  // nhost.graphql.request returns a promise, so we use await here
-  const todos = await nhost.graphql.request(`
+  // Insert todo
+  // const { data: todo } = await nhost.graphql.request(`
+  //   mutation insert_single_todo {
+  //     insert_todos_one(
+  //       object: {
+  //         name: "Todo - 10"
+  //       }
+  //     ){
+  //       id
+  //       name
+  //     }
+  //   }
+  // `);
+  
+  // console.log("Todo created with the data", todo);
+
+  // Get todos
+  const { data: todos } = await nhost.graphql.request(`
     query {
       todos {
         id
         created_at
         name
         is_completed
+        user_id
       }
     }
   `);
 
   // Print todos to console
-  console.log(JSON.stringify(todos.data, null, 2));
+  console.log(JSON.stringify(todos, null, 2));
 });
 
 request();
